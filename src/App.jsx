12e61c45717c666994840d945bbe3b9e48ac6deb
Manjeet Rathore <20je0536@ee.iitism.ac.in>
2024-11-28 09:15:02 +0530

    Almost Finished

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,3 @@
-import { useState } from 'react';
 import './App.css';
 import Navbar from './components/navbar/Navbar';
 import ToggleButton from './components/toogleButton/ToogleButton';
@@ -6,16 +5,22 @@ import Difficulty from './components/cards/Difficulty';
 import Duration from './components/cards/Duration';
 import Questions from './components/cards/Questions';
 import Carousel from './components/carousel/Carousel'
+import InterviewButton from './components/interviewButton/InterviewButton';
 
 function App() {
   return (
-    <div className="App">
+    <div className="App min-h-screen">
       <Navbar />
       <ToggleButton />
+      <Carousel />
+      <div className='DialogCards'>
       <Difficulty />
       <Duration />
       <Questions />
-      <Carousel />
+      </div>
+      <div className='InterviewButton'>
+        <InterviewButton />
+      </div>
     </div>
   );
 }
---
+++ src/components/interviewButton/InterviewButton.jsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import './interviewButton.css';
+
+const InterviewButton = () => {
+    return (
+        <div
+            className="Frame1000007822"
+            style={{
+                width: 251,
+                height: 60,
+                paddingLeft: 50,
+                paddingRight: 50,
+                paddingTop: 21,
+                paddingBottom: 21,
+            }}
+        >
+            <div
+                className="TakeAiInterview"
+            >
+                Take AI interview
+            </div>
+        </div>
+
+    )
+}
+
+export default InterviewButton;
